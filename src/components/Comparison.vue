cc9b823be223a559bdc6f0c4b33e36f32f32f48c
stu126940@mail.uni-kiel.de <stu126940@mail.uni-kiel.de>
2020-08-26 12:52:36 +0200

    Add basic structure of a  header for time selection

--- src/components/Comparison.vue
+++ src/components/Comparison.vue
@@ -1,98 +1,142 @@
 <template>
-    <b-container v-if="plots.length > 0" class="">
-        <comparison-plot v-for="plot in plots" :key="plot"
-            :sensorRegistry="sensorRegistry"
-            :domainX="domainX"
-            :colors="colorRepository"
-            @remove="removePlot(plot)"
-            @update-domain-x="updateDomainX"
-            class="mb-4"
-            />
-        <b-row class="mb-4">
-            <b-col>
-                <b-button variant="success" @click="addPlot">
-                    Add Plot
-                </b-button>
-            </b-col>
-        </b-row>
+  <div>
+    <div>
+      <comparison-setting-header @updatedViewSettings="changeViewSettings"></comparison-setting-header>
+    </div>
+    <b-container v-if="plots.length > 0" class>
+      <comparison-plot
+        ref="comp"
+        v-for="plot in plots"
+        :key="plot"
+        :sensorRegistry="sensorRegistry"
+        :domainX="domainX"
+        :colors="colorRepository"
+        @remove="removePlot(plot)"
+        @update-domain-x="updateDomainX"
+        class="mb-4"
+      />
+      <b-row class="mb-4">
+        <b-col>
+          <b-button variant="success" @click="addPlot">Add Plot</b-button>
+        </b-col>
+      </b-row>
     </b-container>
     <b-container v-else class="empty-container text-center">
-        <font-awesome-icon icon="chart-line" class="empty-icon" />
-        <br />
-        <b-button variant="success" size="lg" @click="addPlot">Add Plot</b-button>
+      <font-awesome-icon icon="chart-line" class="empty-icon" />
+      <br />
+      <b-button variant="success" size="lg" @click="addPlot">Add Plot</b-button>
     </b-container>
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, Watch } from "vue-property-decorator"
-import { Sensor, AggregatedSensor, MachineSensor, SensorRegistry } from '../SensorRegistry'
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+import {
+  Sensor,
+  AggregatedSensor,
+  MachineSensor,
+  SensorRegistry
+} from "../SensorRegistry";
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import ComparisonPlot from './ComparisonPlot.vue';
+import ComparisonPlot from "./ComparisonPlot.vue";
+import ComparisonSettingHeader from "./ComparisonSettingHeader.vue";
 import ColorRepository from "../ColorRepository";
+import { faClosedCaptioning } from "@fortawesome/free-solid-svg-icons";
+import ComparisonSettingBar from "./../model/comparison-view-settings";
+import DateRangePicker from "vue2-daterange-picker";
+import "vue2-daterange-picker/dist/vue2-daterange-picker.css";
 
 @Component({
-    components: {
-        ComparisonPlot
+  components: {
+    ComparisonPlot,
+    ComparisonSettingHeader,
+    DateRangePicker
+  },
+  filters: {
+    date(value: any) {
+      if (!value) return "";
+
+      let options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric"
+      };
+      return Intl.DateTimeFormat("en-EN", options).format(value);
     }
+  }
 })
 export default class Comparision extends Vue {
+  @Prop({ required: true })
+  sensorRegistry!: SensorRegistry;
 
-    @Prop({ required: true })
-    sensorRegistry!: SensorRegistry
+  private dateRange = { startDate: "", endDate: "" };
+  private from: any;
+  private to: any;
+  private resolution: number = 1;
 
-    private plots = new Array<number>()
+  private plots = new Array<number>();
 
-    private nextPlotId = 0
+  private nextPlotId = 0;
 
-    colorRepository = new ColorRepository()
+  colorRepository = new ColorRepository();
 
-    domainX = new Array<Date>()
+  domainX = new Array<Date>();
 
-    addPlot() {
-        this.plots.push(this.nextPlotId)
-        this.nextPlotId++
+  addPlot() {
+    this.plots.push(this.nextPlotId);
+    this.nextPlotId++;
+  }
+
+  removePlot(plot: number) {
+    this.plots.splice(this.plots.indexOf(plot), 1);
+  }
+
+  updateDomainX(domainX?: Array<Date>) {
+    if (domainX) {
+      this.domainX = domainX;
+      //console.log(this.domainX)
     }
+  }
 
-    removePlot(plot: number) {
-        this.plots.splice(this.plots.indexOf(plot), 1)
-    }
+  private changeViewSettings(
+    dateRange: { startDate: number; endDate: number },
+    resolution: any
+  ) {
+    this.$refs.comp[0].updateDataSet(dateRange, resolution);
+  }
 
-    updateDomainX(domainX?: Array<Date>) {
-        if (domainX) {
-            this.domainX = domainX
-            //console.log(this.domainX)
-        }
-    }
-
+  private setResolution() {}
 }
 
 class Plot {
+  readonly dataSets = new Array<DataSet>();
 
-    readonly dataSets = new Array<DataSet>()
+  newDataSet = "";
 
-    newDataSet = ""
-
-    constructor (readonly id: number) {}
+  constructor(readonly id: number) {}
 }
 
 class DataSet {
-
-    constructor (readonly sensor: string) {}
-
+  constructor(readonly sensor: string) {}
 }
-
 </script>
 
 <style scoped>
-    .empty-container {
-        padding-top: 4em;
-    }
-    .empty-icon {
-        color: rgba(0, 0, 0, 0.2);
-        font-size: 16em;
-    }
+.empty-container {
+  padding-top: 4em;
+}
+.empty-icon {
+  color: rgba(0, 0, 0, 0.2);
+  font-size: 16em;
+}
+
+.center {
+  text-align: center;
+  margin: auto;
+}
 </style>
---
+++ src/components/ComparisonSettingHeader.vue
@@ -0,0 +1,83 @@
+<template>
+  <b-row class="text-center">
+    <b-col>
+      <b-container fluid class="center">
+        <date-range-picker
+          ref="picker"
+          :timePicker="true"
+          :showWeekNumbers="true"
+          :autoApply="true"
+          v-model="dateRange"
+          @update="updateValues"
+        >
+          <template
+            v-slot:input="picker"
+            style="min-width: 350px;"
+          >{{ picker.startDate | date }} - {{ picker.endDate | date }}</template>
+        </date-range-picker>
+      </b-container>
+    </b-col>
+    <b-col>
+      <b-button variant="success" @click="setResolution">Set resolution</b-button>
+    </b-col>
+  </b-row>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+import ComparisonSettingBar from "./../model/comparison-view-settings";
+
+import DateRangePicker from "vue2-daterange-picker";
+import "vue2-daterange-picker/dist/vue2-daterange-picker.css";
+import ComparisonViewSettings from "./../model/comparison-view-settings";
+
+@Component({
+  components: {
+    DateRangePicker
+  },
+  filters: {
+    date(value: any) {
+      if (!value) return "";
+
+      let options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric"
+      };
+      return Intl.DateTimeFormat("en-EN", options).format(value);
+    }
+  }
+})
+export default class comparisonSettingBar extends Vue {
+  private dateRange = {
+    startDate: "",
+    endDate: ""
+  };
+
+  private resolution: number = 0;
+
+  private updateValues() {
+    this.$emit(
+      "updatedViewSettings",
+      this.dateRange.startDate,
+      this.dateRange.endDate,
+      0
+    );
+    this.updateValues1;
+  }
+  private updateValues1() {
+    console.log("Header update");
+  }
+
+  setResolution() {}
+}
+</script>
+
+<style scoped>
+</style>
