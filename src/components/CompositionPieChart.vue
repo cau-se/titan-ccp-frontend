a7766cabed252a7e053e0b77d7ef2fa664212092
Sören Henning <soeren.henning@email.uni-kiel.de>
2020-11-16 13:19:35 +0100

    Fix indentation

--- src/components/CompositionPieChart.vue
+++ src/components/CompositionPieChart.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">Composition</h5>
-            <loading-spinner :is-loading="isLoading" :is-error="isError">
-                <div class="c3-container"></div>
-            </loading-spinner>
-        </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Composition</h5>
+      <loading-spinner :is-loading="isLoading" :is-error="isError">
+        <div class="c3-container"></div>
+      </loading-spinner>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -19,83 +19,81 @@ import 'c3/c3.css'
 import TimeMode from "../model/time-mode";
 
 @Component({
-    components: {
-        LoadingSpinner
-    }
+  components: {
+    LoadingSpinner
+  }
 })
 export default class CompositionPieChart extends Vue {
+  @Prop({ required: true }) sensor!: AggregatedSensor
 
-    @Prop({ required: true }) sensor!: AggregatedSensor
+  @Prop({ required: true }) timeMode!: TimeMode;
 
-    @Prop({ required: true }) timeMode!: TimeMode;
+  private isLoading = false
+  private isError = false
 
-    private isLoading = false
-    private isError = false
+  private chart!: ChartAPI
 
-    private chart!: ChartAPI
+  mounted() {
+    this.chart = generate({
+      bindto: this.$el.querySelector(".c3-container") as HTMLElement,
+      data: {
+        columns: [],
+        type : 'pie'
+      },
+      tooltip: {
+        show: false
+      }
+    })
+    this.updateChart()
+  }
 
-    mounted() {
-        this.chart = generate({
-            bindto: this.$el.querySelector(".c3-container") as HTMLElement,
-            data: {
-                columns: [],
-                type : 'pie'
-            },
-            tooltip: {
-                show: false
-            }
+  @Watch('sensor')
+  onSensorChanged() {
+    this.updateChart()
+  }
+
+  @Watch("timeMode")
+  onTimeModeChanged() {
+    this.updateChart();
+  }
+
+  private updateChart() {
+    this.isLoading = true
+  
+    let to = this.timeMode.getTime();
+
+    Promise.all(this.sensor.children.map(child => {
+      let resource = child instanceof AggregatedSensor ? 'active-power/aggregated' : 'active-power/raw'
+      return HTTP.get(resource + '/' + child.identifier + '/latest?to=' + to.toMillis())
+        .then(response => {
+          // JSON responses are automatically parsed.
+          let value
+          if (response.data.length <= 0) {
+            value = 0
+          } else if (child instanceof AggregatedSensor) {
+            value = response.data[0].sumInW
+          } else {
+            value = response.data[0].valueInW
+          }
+          return <[string, number]> [child.title, value]
         })
-        this.updateChart()
-    }
-
-    @Watch('sensor')
-    onSensorChanged() {
-        this.updateChart()
-    }
-
-    @Watch("timeMode")
-    onTimeModeChanged() {
-        this.updateChart();
-    }
-
-    private updateChart() {
-        this.isLoading = true
-        
-        let to = this.timeMode.getTime();
-
-        Promise.all(this.sensor.children.map(child => {
-            let resource = child instanceof AggregatedSensor ? 'active-power/aggregated' : 'active-power/raw'
-            return HTTP.get(resource + '/' + child.identifier + '/latest?to=' + to.toMillis())
-                .then(response => {
-                    // JSON responses are automatically parsed.
-                    let value
-                    if (response.data.length <= 0) {
-                        value = 0
-                    } else if (child instanceof AggregatedSensor) {
-                        value = response.data[0].sumInW
-                    } else {
-                        value = response.data[0].valueInW
-                    }
-                    return <[string, number]> [child.title, value]
-                })
-        })).catch(e => {
-            console.error(e)
-            this.isError = true
-            return []
-        }).then(columns => {
-            this.isLoading = false
-            this.chart.load({
-                columns: columns,
-                unload: true
-            })
-        })
-    }
-
+    })).catch(e => {
+      console.error(e)
+      this.isError = true
+      return []
+    }).then(columns => {
+      this.isLoading = false
+      this.chart.load({
+        columns: columns,
+        unload: true
+      })
+    })
+  }
 }
 </script>
 
 <style scoped>
-    .c3-container {
-        height: 300px;
-    }
+  .c3-container {
+    height: 300px;
+  }
 </style>
