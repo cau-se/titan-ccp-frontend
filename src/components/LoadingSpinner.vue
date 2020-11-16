cda407cd966cfd53bb789b284d2c1e7e4b6a42a1
Sören Henning <soeren.henning@email.uni-kiel.de>
2020-11-16 13:05:20 +0100

    Fix indentation

--- src/components/LoadingSpinner.vue
+++ src/components/LoadingSpinner.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="wrapper">
-        <div v-if="isLoading" class="icon-container">
-            <font-awesome-icon icon="cog" spin class="icon" />
-        </div>
-        <div v-else-if="isError"  class="icon-container">
-            <font-awesome-icon icon="times" class="icon" />
-            <p class="text-muted">Could not connect to server</p>
-        </div>
-        <div :class="{ hidden: isLoading||isError }">
-            <slot></slot>
-        </div>
+  <div class="wrapper">
+    <div v-if="isLoading" class="icon-container">
+      <font-awesome-icon icon="cog" spin class="icon" />
     </div>
+    <div v-else-if="isError"  class="icon-container">
+      <font-awesome-icon icon="times" class="icon" />
+      <p class="text-muted">Could not connect to server</p>
+    </div>
+    <div :class="{ hidden: isLoading||isError }">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -22,30 +22,28 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 @Component
 export default class LoadingSpinner extends Vue {
+  @Prop({ required: true }) isLoading!: boolean
 
-    @Prop({ required: true }) isLoading!: boolean
-
-    @Prop() isError = false
-
+  @Prop() isError = false
 }
 </script>
 
 <style scoped>
-    .wrapper {
-        position: relative;
-    }
-    .icon-container {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        margin-top: 10%;
-        text-align: center;
-    }
-    .icon {
-        font-size: 8em;
-        color: rgba(0,0,0,0.5);
-    }
-    .hidden {
-        visibility: hidden;
-    }
+  .wrapper {
+    position: relative;
+  }
+  .icon-container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    margin-top: 10%;
+    text-align: center;
+  }
+  .icon {
+    font-size: 8em;
+    color: rgba(0,0,0,0.5);
+  }
+  .hidden {
+    visibility: hidden;
+  }
 </style>
