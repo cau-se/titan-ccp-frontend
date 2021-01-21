1e4cbe0c3b9425094a808849e0e32c45ebdad3d4
Sören Henning <soeren.henning@email.uni-kiel.de>
2021-01-21 19:04:38 +0100

    Add SASS loader

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,19 @@ module.exports = {
       title: 'Titan Control Center'
     }
   },
+
   devServer: {
     proxy: {
       '/api': {
         target: 'http://localhost:8080'
       }
+    }
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: []
     }
   }
 }
